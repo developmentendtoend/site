820f3d55f3ef083bd9a74416d938dd55f3e58aa2
tw1ttt3r <tw1ttt3r@gmail.com>
2023-04-17 22:03:54 -0600

    feat: refactored navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,23 @@
 import { FC, useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import useSocialMedia from "@/lib/useSocialMedia";
 
-const Navbar: FC<any> = ({ socialNetworks }: any) => {
+const Navbar: FC<any> = () => {
 
-  const [ socialNetworksInfo, setSocialNetworksInfo ] = useState([]);
+  const { allSocialMedia } = useSocialMedia();
+
+  const [ socialNetworksInfo, setSocialNetworksInfo ] = useState<Array<any>>([]);
 
   useEffect(() => {
-    if (!!socialNetworks) {
-      setSocialNetworksInfo(() => socialNetworks);
+    if (!!allSocialMedia) {
+      setSocialNetworksInfo([ ...allSocialMedia ]);
     }
-  }, [ socialNetworks ])
+  }, [ allSocialMedia ])
 
   return <section className="fixed top-0 left-0 w-screen">
     {
-      !!socialNetworksInfo.length && <section className="bg-[#C2302F] flex gap-6 justify-end p-4">
+      !!socialNetworksInfo.length && <section className="bg-[#C2302F] flex gap-6 justify-end p-4 drop-shadow-navbar pr-6">
         {
           socialNetworksInfo.map( ({ src, alt, url }: any, i: number) => <Link key={`social-${i}`} href={url} legacyBehavior>
               <a target="_blank">
@@ -25,6 +28,16 @@ const Navbar: FC<any> = ({ socialNetworks }: any) => {
         }
       </section>
     }
+    <section>
+      <ul className="flex justify-end">
+        <li className="p-6">Acerca de</li>
+        <li className="p-6">Servicios</li>
+        <li className="p-6">Alianzas</li>
+        <li className="p-6">Clientes y Casos de Éxito</li>
+        <li className="p-6">Carrera</li>
+        <li className="p-6">Contacto</li>
+      </ul>
+    </section>
   </section>
 }
 
